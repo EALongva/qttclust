72e3d818d340317a44a66b67b3b13c5db60e2c10
EALongva <e.a.longva@fys.uio.no>
2022-06-14 03:04:42 +0200

    njord storage test

--- pushtest.py
+++ pushtest.py
@@ -1,3 +1,28 @@
 # testing pushing programs to gitclust and syncing with munin.uio.no
 
+from lwQTT import *
+
+# test read write to njord
+
+# idé til en annen gang -> hvis du er usikker på saving loading paths, lag en test slik som denne
+# med et enkelt array, skriv inn path fra command line, kjør testen, sett prgm på vent og gi
+# resultat fra testen og så prompt continue :)
+
+savearray = np.arange(9)
+print('saved array=', savearray)
+
+#path = 'data/' # local path
+
+path = '../../../njord/erlenalo/data/'
+
+savename = path + 'savearray'
+np.save(savename, savearray)
+
+loadname = savename + '.npy'
+loadarray= np.load(loadname)
+
+print('load array=', loadarray)
+
 print('test success')
+
+
